281a87764de357f848808509ce49bb0f5e795225
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-19 00:19:24 +0000

    add header with quick intro and links

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export function Header(): JSX.Element {
+  return (
+    <>
+      <header>
+        <h3>Tinashe Gutu - Junio Software Developer</h3>
+        <p>I am web and software developer based in London</p>
+        <ul>
+          <a href="https://github.com/tinashe-gutu">Github</a>
+          <a href="https://www.linkedin.com/in/tinashe-gutu-24820818b/">
+            LinkedIn
+          </a>
+        </ul>
+      </header>
+    </>
+  );
+}
