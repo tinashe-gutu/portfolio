1b4cdaacb8c017b47d65914d0f8eefb75b1d2b22
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-01-19 00:19:54 +0000

    add projects so far to main component

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,10 @@
+import { ProjectList } from "./ProjectList";
+
+export function MainContent(): JSX.Element {
+  return (
+    <main>
+      <h2>My Projects</h2>
+      <ProjectList />
+    </main>
+  );
+}
