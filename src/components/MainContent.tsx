f5c26c6a90d82381f83fa2ca900aa07543063391
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-02-09 20:24:11 +0000

    remove header tag

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,7 +3,6 @@ import { ProjectList } from "./ProjectList";
 export function MainContent(): JSX.Element {
   return (
     <main>
-      <h2>My Projects</h2>
       <ProjectList />
     </main>
   );
