816e7c7ef1620d6b7c115d1bef6b36e9b66372d2
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-02-09 20:23:45 +0000

    add react router and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { MainContent } from "./components/MainContent";
 import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { BrowserRouter } from "react-router-dom";
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
