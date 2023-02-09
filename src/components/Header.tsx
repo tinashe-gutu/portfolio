1b67bf3ab8e2ecf11d6e1e3e4f5db3270ec3f5ca
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-02-09 20:23:23 +0000

    add an about section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,59 @@
 export function Header(): JSX.Element {
   return (
-    <>
-      <header>
-        <h3>Tinashe Gutu - Junio Software Developer</h3>
-        <p>I am web and software developer based in London</p>
-        <ul>
-          <a href="https://github.com/tinashe-gutu">Github</a>
-          <a href="https://www.linkedin.com/in/tinashe-gutu-24820818b/">
+    <section className="header">
+      <header className="intro">
+        <div className="short-intro-container">
+          <h1 className="name">Hi, I'm Tinashe</h1>
+          <p className="short-intro">
+            From teaching mathematics to solving software engineering puzzles. I
+            am a <b>junior software engineer</b> with a keen eye for readable
+            and realiable solutions. My current focus is in building full stack
+            web application using the <b>PERN</b> stack at Academy Tech
+          </p>
+        </div>
+      </header>
+      <div id="about">
+        <h1>About</h1>
+        <p>
+          Hello my name is Tinashe and i enjoy spotting real life opportunities
+          and problems that can be solved through tech. My interest in software
+          began in 2020 during my final year studying aerospace engineering
+          where i stumbled onto an online course deliverd by havard called CS50
+          and i became obssessed with this new world of problem solving and
+          thinking!
+        </p>
+        <p>
+          Fast-forward to today and some self taught online courses later, I
+          have retrained as a software engineer through{" "}
+          <a href="https://academy.tech/" target="_blank">
+            Academy Tech's
+          </a>{" "}
+          emersive program with a curriculum that focuses on leadership and
+          industry readiness.
+        </p>
+        <p>Here are few technologies i have been working with recently:</p>
+        <ul className="technologies">
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>React</li>
+          <li>ExpressJS</li>
+          <li>PostgreSQL</li>
+          <li>JEST</li>
+          <li>Cypress</li>
+          <li>CI/CD</li>
+        </ul>
+        <ul className="socials">
+          <a href="https://github.com/tinashe-gutu" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tinashe-gutu-24820818b/"
+            target="_blank"
+          >
             LinkedIn
           </a>
         </ul>
-      </header>
-    </>
+      </div>
+    </section>
   );
 }
