6db2e54c09c744e6d5ae7c6dc32ad33375129cec
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-02-09 20:24:54 +0000

    add contact details

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,15 @@
 export function Footer(): JSX.Element {
-  return <h4>Tinashe Gutu</h4>;
+  return (
+    <div id="contact">
+      <h1 className="outro">Get In Touch</h1>
+      <h2>
+        Thank you for visiting my portfolio. I hope you found what you were
+        looking for. If you'd like to discuss potential opportunities or connect
+        further, I am currently looking for a new role and would love to here
+        from you
+      </h2>
+      <h3>tinashegutu2011@gmail.com</h3>
+      <h3>07412041397</h3>
+    </div>
+  );
 }
