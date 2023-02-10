5950b79827feee8eefb20535a4d993a6d79f08eb
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-02-10 15:10:07 +0000

    format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ export function Footer(): JSX.Element {
       <h2>
         Thank you for visiting my portfolio. I hope you found what you were
         looking for. If you'd like to discuss potential opportunities or connect
-        further, I am currently looking for a new role and would love to here
-        from you
+        further, I am currently looking for a new role and would love to hear
+        from you.
       </h2>
       <h3>tinashegutu2011@gmail.com</h3>
       <h3>07412041397</h3>
