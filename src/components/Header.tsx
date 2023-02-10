090b032b0adc8687b8ac65cb2fd0f2d500f89d77
Tinashe Gutu <tinashegutu2011@gmail.com>
2023-02-10 15:09:36 +0000

    rewrite descriptions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header(): JSX.Element {
         <div className="short-intro-container">
           <h1 className="name">Hi, I'm Tinashe</h1>
           <p className="short-intro">
-            From teaching mathematics to solving software engineering puzzles. I
+            From teaching Mathematics to solving software engineering puzzles. I
             am a <b>junior software engineer</b> with a keen eye for readable
             and realiable solutions. My current focus is in building full stack
             web application using the <b>PERN</b> stack at Academy Tech
@@ -15,21 +15,18 @@ export function Header(): JSX.Element {
       <div id="about">
         <h1>About</h1>
         <p>
-          Hello my name is Tinashe and I enjoy spotting real life opportunities
+          Hello, my name is Tinashe and I enjoy spotting real life opportunities
           and problems that can be solved through tech. My interest in software
-          began in 2020 during my final year studying aerospace engineering
-          where I stumbled onto an online course deliverd by havard called CS50
+          began in 2020 during my final year studying Aerospace Engineering
+          where I stumbled onto an online course deliverd by Havard called CS50
           and I became obssessed with this new world of problem solving and
           thinking!
         </p>
         <p>
           Fast-forward to today and some self taught online courses later, I
-          have retrained as a software engineer through{" "}
-          <a href="https://academy.tech/" target="_blank" rel="noreferrer">
-            Academy Tech's
-          </a>{" "}
-          emersive program with a curriculum that focuses on leadership and
-          industry readiness.
+          have retrained as a software engineer through Academy Tech's emersive
+          program with a curriculum that focuses on leadership and industry
+          readiness.
         </p>
         <p>Here are few technologies i have been working with recently:</p>
         <ul className="technologies">
